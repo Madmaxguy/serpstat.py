f37bbd7327c07884d5ed114d746ba0f503aafd03
madmaguy <madmax_w3@hotmail.com>
2018-10-23 22:33:34 +0300

    json example

--- json/parse.py
+++ json/parse.py
@@ -1,38 +1,127 @@
 #!/usr/bin/python
 import json
-import pprint
+import openpyxl
+from datetime import datetime
+from tkinter import *
+import random
+from tkinter import Tk, Label, Button, Entry, StringVar, DISABLED, NORMAL, END, W, E
+
+
+class Calculator:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
+
+        self.total = 0
+        self.entered_number = 0
+
+        self.total_label_text = IntVar()
+
+        #vcmd = master.register(self.validate) # we have to wrap the command
+        self.labelT = Label(master, text="Token:")
+        self.labelF = Label(master, text="File Path:")
+        self.entry1 = Entry(master) #, validate="key", validatecommand=(vcmd, '%P'))
+        self.entry2 = Entry(master)
+
+        self.ok_button = Button(master, text="OK")
+        self.choose_button = Button(master, text="Choose file", command=lambda: self.choose())
+        #self.add_button = Button(master, text="+", command=lambda: self.update("add"))
+        #self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
+        #self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
+
+        # LAYOUT
+
+        self.entry1.grid(row=1, column=1, columnspan=3, sticky=W+E)
+        self.entry2.grid(row=2, column=1, columnspan=3, sticky=W + E)
+        self.labelT.grid(row=1, column=0)
+        self.labelF.grid(row=2, column=0)
+
+        self.ok_button.grid(row=3, column=0)
+        self.choose_button.grid(row=2, column=5)
+
+    def choose(self):
+        return True
+
+    def ok(self):
+        token = self.entry1.get()
+        filename = self.entry2.get()
+
+    def update(self, method):
+        if method == "add":
+            self.total += self.entered_number
+        elif method == "subtract":
+            self.total -= self.entered_number
+        else: # reset
+            self.total = 0
+
+        self.total_label_text.set(self.total)
+        self.entry.delete(0, END)
+
+root = Tk()
+my_gui = Calculator(root)
+root.mainloop()
+
+print(my_gui.token)
+
+
+#root = Tk()
+#d = MyDialog(root)
+#root.mainloop()
+
+# Output
+def output_to_excel():
+    now = datetime.now()
+    output_file = 'results_' + now.strftime("%Y-%m-%d_%H:%M:%S") + '.xlsx'
+    wb = openpyxl.Workbook()
+    engine = "g_us"
+    sheetname = "Top_keyword" + engine
+    wb.create_sheet(sheetname)
+    sheet = wb.get_sheet_by_name(sheetname)
+
+    sheet.cell(row=1 + 1, column=1).value = 'Keyword'
+    sheet.cell(row=1, column=1).font = openpyxl.styles.Font(bold=True)
+    sheet.cell(row=1, column=2).value = 'Query engine'
+    sheet.cell(row=1, column=2).font = openpyxl.styles.Font(bold=True)
+    sheet.cell(row=1, column=3).value = 'Response Message'
+    sheet.cell(row=1, column=3).font = openpyxl.styles.Font(bold=True)
+    sheet.cell(row=1, column=4).value = 'Queries count'
+    sheet.cell(row=1, column=4).font = openpyxl.styles.Font(bold=True)
+    sheet.cell(row=1, column=5).value = 'Found Results'
+    sheet.cell(row=1, column=5).font = openpyxl.styles.Font(bold=True)
+    sheet.cell(row=1, column=6).value = 'Domains'
+    sheet.cell(row=1, column=6).font = openpyxl.styles.Font(bold=True)
+    sheet.cell(row=1, column=7).value = 'Full URLs'
+    sheet.cell(row=1, column=7).font = openpyxl.styles.Font(bold=True)
+    # ws = wb.activate()
+
+    wb.save(output_file)
 
 file = open("json.example","r")
 #print(file.read())
 
 jsondata = json.loads(file.read())
-# print(jsondata)
-
-# output to file example
-# output = open("json.example2", "w")
-# pprint.pprint(jsondata, output)
-
-# left_lines = jsondata["left_lines"]
-# print(left_lines)
-# easier than above
 
 domains = ""
-# print("\ngetting all domains:")
 for item in jsondata['result']['top']:
     domains += item.get("domain") + ","
-
-# print(domains)
-
 urls = ""
-# print("\ngetting all URLS:")
 for item in jsondata['result']['top']:
     urls += item.get("url") + ","
-
-# print(urls)
-
 print(jsondata['left_lines'])
 print(jsondata['result']['results'])
-# print(jsondata['result']['top'][0]['domain'] + ", " + jsondata['result']['top'][0]['url'])
 print("found_domains:" + domains)
 print("found_urls:" + urls)
-print()
+print(len(jsondata['result']['top']))
+if jsondata['status_code'] == 200:
+    print("status code is 200:")
+    print(jsondata['status_code'])
+
+print(jsondata['status_msg'])
+status_msg = "%i"% (jsondata['status_code']) + ", " + jsondata['status_msg']
+print(status_msg)
+
+
+
+# Output
+# output_to_excel()
--- serpstat.py
+++ serpstat.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
-import codecs
 import json
-import pprint
-import os
-import pandas as pd
 import urllib.request as urlrequest
 from urllib.parse import urlencode
 from openpyxl import load_workbook
-
+from datetime import datetime
+import openpyxl
 from tkinter import *
-#import tkinter as tk
 from tkinter import filedialog
+import bpy
 
 host = 'http://api.serpstat.com/v3'
+method = 'keyword_top'
+se = 'g_ru'
 
 ## Get filename
 root = Tk()
@@ -20,68 +19,93 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 print(file_path)
 
-## Get Token
-#def show_entry_fields():
-#   print("Token: %s\n" % (e1.get()))
-
+# Inputting Token for API
 master = Tk()
 Label(master, text="Token").grid(row=0)
 #Label(master, text="Last Name").grid(row=1)
-
 e1 = Entry(master)
 #e2 = Entry(master)
-
 e1.grid(row=0, column=1)
 #e2.grid(row=1, column=1)
-
 Button(master, text='OK', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
 #Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
 mainloop( )
 my_token = e1.get()
-
 print(my_token)
 
-#exit()
-
-## Load in the workbook
-wb = load_workbook(file_path)
-sheet = wb.get_sheet_by_name('Лист1')
+# Getting Input keywords
+wb1 = load_workbook(file_path)
+sheet = wb1.get_sheet_by_name('Лист1')
 sheet.title
 
 
-#for i in range(1, 100):
-#    print(i, sheet.cell(row=i, column=1).value)
-     
-     
-method = 'keyword_top'
+# Writing output file
+now = datetime.now()
+output_file = 'results_' + now.strftime("%Y-%m-%d_%H:%M:%S") + '.xlsx'
+wb2 = openpyxl.Workbook()
+sheetname = method + "_" + se
+wb2.create_sheet("Top_keyword")
+sheet = wb2.get_sheet_by_name('Top_keyword')
+
+sheet.cell(row=1, column=1).value = 'Keyword'
+sheet.cell(row=1, column=1).font = openpyxl.styles.Font(bold=True)
+sheet.cell(row=1, column=2).value = 'Response Message'
+sheet.cell(row=1, column=2).font = openpyxl.styles.Font(bold=True)
+sheet.cell(row=1, column=3).value = 'Found Results'
+sheet.cell(row=1, column=3).font = openpyxl.styles.Font(bold=True)
+sheet.cell(row=1, column=4).value = 'Domains'
+sheet.cell(row=1, column=4).font = openpyxl.styles.Font(bold=True)
+sheet.cell(row=1, column=5).value = 'Full URLs'
+sheet.cell(row=1, column=5).font = openpyxl.styles.Font(bold=True)
+#ws = wb.activate()
+
+
 
 for i in range(1, 100):
     qr = sheet.cell(row=i, column=1).value.replace(" ", "%20")
     params = {
         'query': qr,  # string for get info
-        'se': 'g_ru',  # string search engine
+        'se': se ,  # string search engine
         'token': my_token,  # string personal token
     }
-
     api_url = "{host}/{method}?{params}".format(
         host=host,
         method=method,
         params=urlencode(params)
     )
-
     try:
         json_data = urlrequest.urlopen(api_url).read()
     except Exception as e0:
         print("API request error: {error}".format(error=e0))
 
-    data = json.loads(json_data)
-    #pprint.pprint(data)
-    #print(params)
+    # Parsing JSON
+    jsondata = json.loads(json_data)
+    # Extracting data from JSON
+    domains = ""
+    urls = ""
+    left_lines = jsondata['left_lines']
+    status_msg = "%i" % (jsondata['status_code']) + ", " + jsondata['status_msg']
+    results_found = ""
 
-    #getting output for requests
+    if jsondata['status_code'] == 200:
+        results_found = jsondata['result']['results']
+        # Getting domains
+        for item in jsondata['result']['top']:
+            domains += item.get("domain") + ","
+        # Getting URLs
+        for item in jsondata['result']['top']:
+            urls += item.get("url") + ","
+
     print(qr)
-    print(status_code)
     print(status_msg)
     print(results_found)
     print(domains)
     print(urls)
+    print("lines left: " + results_found)
+    sheet.cell(row=i + 1, column=1).value = qr
+    sheet.cell(row=i + 1, column=2).value = status_msg
+    sheet.cell(row=i + 1, column=3).value = results_found
+    sheet.cell(row=i + 1, column=4).value = domains
+    sheet.cell(row=i + 1, column=5).value = urls
+
+wb2.save(output_file)
